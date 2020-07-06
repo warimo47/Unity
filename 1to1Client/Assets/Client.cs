54af618cdfa76981894e7b65a12b181982368cd9
KangsanBae <warimo47@naver.com>
2020-07-06 17:52:02 +0900

    Add Unity TCP 1:1 Communication

---
+++ 1to1Client/Assets/Client.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Net.Sockets;
+using System.IO;
+using System.Runtime.InteropServices;
+using UnityEngine;
+using UnityEngine.UI;
+
+#region [Packet define]
+[Serializable]
+[StructLayout(LayoutKind.Sequential, Pack = 1)]
+public unsafe struct IF_SC100
+{
+    public byte header;
+    public fixed char IP[30];
+    public double latitude;
+    public double longitude;
+    public byte objType;
+    public fixed char time[20];
+    public float objX1;
+    public float objY1;
+    public float objX2;
+    public float objY2;
+};
+
+[Serializable]
+[StructLayout(LayoutKind.Sequential, Pack = 1)]
+public unsafe struct IF_SC200
+{
+    public byte header;
+    public fixed char deviceID[8];
+    public fixed char time[10];
+    public fixed char fireStatusInformation[10];
+    public fixed char deviceStatusInformation[10];
+};
+
+[Serializable]
+[StructLayout(LayoutKind.Sequential, Pack = 1)]
+public unsafe struct IF_SC300
+{
+    public byte header;
+    public fixed char deviceID[8];
+    public fixed char time[10];
+    public fixed char eventStatusInformation[10];
+    public fixed char deviceStatusInformation[10];
+    public fixed char emergencySituationInformation[10];
+};
+#endregion
+
+public class Client : MonoBehaviour
+{
+    private TcpClient client;
+    private NetworkStream ns;
+    private StreamWriter writer;
+
+    private Button btn_sendIFSC100;
+    private Text txt_sendIFSC100;
+
+    private bool isConnect = false;
+
+    private InputField serverIPText;
+    private InputField portText;
+
+    private IF_SC100 if_sc100;
+    private InputField ipf_IF_SC100_IP;
+    private InputField ipf_IF_SC100_latitude;
+    private InputField ipf_IF_SC100_longitude;
+    private InputField ipf_IF_SC100_objType;
+    private InputField ipf_IF_SC100_time;
+    private InputField ipf_IF_SC100_objX1;
+    private InputField ipf_IF_SC100_objY1;
+    private InputField ipf_IF_SC100_objX2;
+    private InputField ipf_IF_SC100_objY2;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        Screen.SetResolution(640, 480, true);
+        
+        Initialize();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+
+    }
+
+    private unsafe void Initialize()
+    {
+        // ===== UI Binding =====
+        serverIPText = GameObject.Find("IPF_ServerIP").GetComponent<InputField>();
+        portText = GameObject.Find("IPF_Port").GetComponent<InputField>();
+        btn_sendIFSC100 = GameObject.Find("BTN_SendIF-SC100").GetComponent<Button>();
+        txt_sendIFSC100 = GameObject.Find("TXT_Connect").GetComponent<Text>();
+
+        ipf_IF_SC100_IP = GameObject.Find("IPF_IF-SC100_IP").GetComponent<InputField>();
+        ipf_IF_SC100_latitude = GameObject.Find("IPF_IF-SC100_latitude").GetComponent<InputField>();
+        ipf_IF_SC100_longitude = GameObject.Find("IPF_IF-SC100_longitude").GetComponent<InputField>();
+        ipf_IF_SC100_objType = GameObject.Find("IPF_IF-SC100_objType").GetComponent<InputField>();
+        ipf_IF_SC100_time = GameObject.Find("IPF_IF-SC100_time").GetComponent<InputField>();
+        ipf_IF_SC100_objX1 = GameObject.Find("IPF_IF-SC100_objX1").GetComponent<InputField>();
+        ipf_IF_SC100_objY1 = GameObject.Find("IPF_IF-SC100_objY1").GetComponent<InputField>();
+        ipf_IF_SC100_objX2 = GameObject.Find("IPF_IF-SC100_objX2").GetComponent<InputField>();
+        ipf_IF_SC100_objY2 = GameObject.Find("IPF_IF-SC100_objY2").GetComponent<InputField>();
+    }
+
+    public void Connect()
+    {
+        try
+        {
+            client = new TcpClient(serverIPText.text, Int32.Parse(portText.text)); // (ip주소 , 포트 번호)
+            ns = client.GetStream();
+            writer = new StreamWriter(ns);
+
+            if (isConnect == false)
+            {
+                btn_sendIFSC100.enabled = true;
+                txt_sendIFSC100.text = "Disconnect";
+                Debug.Log("Server connet");
+                isConnect = true;
+            }
+            else
+            {
+                btn_sendIFSC100.enabled = false;
+                txt_sendIFSC100.text = "Connect";
+                Debug.Log("Server disconnet");
+                isConnect = false;
+            }
+        }
+        catch
+        {
+            Debug.Log("Connect fail");
+        }
+    }
+
+    public unsafe void SendIF_SC100()
+    {
+        // ===== IF-SC100 value copy =====
+        if_sc100.header = 1;
+        for (int i = 0; i < ipf_IF_SC100_IP.text.Length; ++i)
+        {
+            if_sc100.IP[i] = ipf_IF_SC100_IP.text[i];
+        }
+        if_sc100.latitude = double.Parse(ipf_IF_SC100_latitude.text);
+        if_sc100.longitude = double.Parse(ipf_IF_SC100_longitude.text);
+        if_sc100.objType = byte.Parse(ipf_IF_SC100_objType.text);
+        for (int i = 0; i < ipf_IF_SC100_time.text.Length; ++i)
+        {
+            if_sc100.time[i] = ipf_IF_SC100_time.text[i];
+        }
+        if_sc100.objX1 = float.Parse(ipf_IF_SC100_objX1.text);
+        if_sc100.objY1 = float.Parse(ipf_IF_SC100_objY1.text);
+        if_sc100.objX2 = float.Parse(ipf_IF_SC100_objX2.text);
+        if_sc100.objY2 = float.Parse(ipf_IF_SC100_objY2.text);
+
+        if (ns.CanWrite == true)
+        {
+            ns.Write(StructureToByteArray(if_sc100), 0, sizeof(IF_SC100));
+            Debug.Log("Send IF_SC100");
+        }
+        else
+        {
+            Debug.Log("Can not write");
+        }
+    }
+
+    private void Close()
+    {
+        writer.Close();
+        client.Close();
+    }
+
+    private static byte[] StructureToByteArray(IF_SC100 if_sc100)
+    {
+        byte[] bb = new byte[Marshal.SizeOf(if_sc100)];
+        GCHandle gch = GCHandle.Alloc(bb, GCHandleType.Pinned);
+        Marshal.StructureToPtr(if_sc100, gch.AddrOfPinnedObject(), false);
+        gch.Free();
+        return bb;
+    }
+
+    private static byte[] StructureToByteArray(IF_SC200 if_sc200)
+    {
+        byte[] bb = new byte[Marshal.SizeOf(if_sc200)];
+        GCHandle gch = GCHandle.Alloc(bb, GCHandleType.Pinned);
+        Marshal.StructureToPtr(if_sc200, gch.AddrOfPinnedObject(), false);
+        gch.Free();
+        return bb;
+    }
+}
---
+++ 1to1Server/Assets/Server.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.UI;
+
+#region [Packet define]
+[Serializable]
+[StructLayout(LayoutKind.Sequential, Pack = 1)]
+public unsafe struct IF_SC100
+{
+    public byte header;
+    public fixed char IP[30];
+    public double latitude;
+    public double longitude;
+    public byte objType;
+    public fixed char time[20];
+    public float objX1;
+    public float objY1;
+    public float objX2;
+    public float objY2;
+};
+
+[Serializable]
+[StructLayout(LayoutKind.Sequential, Pack = 1)]
+public unsafe struct IF_SC200
+{
+    public byte header;
+    public fixed char deviceID[8];
+    public fixed char time[10];
+    public fixed char fireStatusInformation[10];
+    public fixed char deviceStatusInformation[10];
+};
+
+[Serializable]
+[StructLayout(LayoutKind.Sequential, Pack = 1)]
+public unsafe struct IF_SC300
+{
+    public byte header;
+    public fixed char deviceID[8];
+    public fixed char time[10];
+    public fixed char eventStatusInformation[10];
+    public fixed char deviceStatusInformation[10];
+    public fixed char emergencySituationInformation[10];
+};
+#endregion
+
+public class Server : MonoBehaviour
+{
+    public static int recvBufferSize = 200;
+
+    private TcpListener tcpListener;
+    private Thread acceptThread;
+    private TcpClient tcpClient;
+    private NetworkStream networkStream;
+    private int recvSize = -1;
+    private byte[] recvBuffer = new Byte[recvBufferSize];
+
+    private Text logText = null;
+    private ScrollRect scrollRect = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        logText = GameObject.Find("Log").GetComponent<Text>();
+        scrollRect = GameObject.Find("ScrollView").GetComponent<ScrollRect>();
+
+        tcpListener = new TcpListener(IPAddress.Any, 4211);
+        tcpListener.Start();
+
+        acceptThread = new Thread(new ThreadStart(this.Accept), 8192);
+        acceptThread.Start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Recv();
+    }
+
+    public void Accept()
+    {
+        tcpClient = tcpListener.AcceptTcpClient(); // 클라이언트와 접속
+        networkStream = tcpClient.GetStream();
+    }
+
+    public void Recv()
+    {
+        if (networkStream != null)
+        {
+            if (networkStream.CanRead == true)
+            {
+                recvSize = networkStream.Read(recvBuffer, 0, recvBufferSize);
+
+                // Debug.Log("recvSize = " + recvSize);
+
+                if (recvSize == 0)
+                {
+                    Debug.Log("recvSize = 0");
+                    Quit();
+                }
+
+                ProccessRecv(recvBuffer);
+            }
+        }
+    }
+
+    public unsafe void ProccessRecv(byte[] recvBuffer)
+    {
+        string debugLine = "[" + this.tcpClient.Client.RemoteEndPoint.ToString() + "] : ";
+
+        switch ((int)recvBuffer[0])
+        {
+            case 1:
+                IF_SC100 if_sc100 = (IF_SC100)ByteArrayToStructure(recvBuffer, typeof(IF_SC100));
+                string ipStr = new string(if_sc100.IP);
+                string timeStr = new string(if_sc100.time);
+                debugLine += ipStr + " " + if_sc100.latitude + " " + if_sc100.longitude + " " + if_sc100.objType + " "
+                    + timeStr + " " + if_sc100.objX1 + " " + if_sc100.objY1 + " " + if_sc100.objX2 + " " + if_sc100.objY2 + "\n";
+                break;
+            case 2:
+                IF_SC200 if_sc200 = (IF_SC200)ByteArrayToStructure(recvBuffer, typeof(IF_SC200));
+                debugLine += "\n";
+                break;
+            case 3:
+                IF_SC300 if_sc300 = (IF_SC300)ByteArrayToStructure(recvBuffer, typeof(IF_SC300));
+                debugLine += "\n";
+                break;
+            default:
+                Debug.Log("recv wrong data " + recvBuffer[0]);
+                break;
+        }
+
+        logText.text += debugLine;
+        scrollRect.verticalNormalizedPosition = 0.0f;
+
+        Debug.Log(debugLine);
+    }
+
+    static public object ByteArrayToStructure(byte[] byteData, Type type)
+    {
+        GCHandle gch = GCHandle.Alloc(byteData, GCHandleType.Pinned);
+        object result = Marshal.PtrToStructure(gch.AddrOfPinnedObject(), type);
+        gch.Free();
+        return result;
+    }
+
+    public void Quit()
+    {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+                Application.Quit();
+        #endif
+    }
+}
