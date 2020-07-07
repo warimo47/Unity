0d32a6006dc48fcf7eb83dc012b9251020a9f3d2
KangsanBae <warimo47@naver.com>
2020-07-07 14:40:48 +0900

    Add CatEscape Add Roulette Add SwipeCar

---
+++ Roulette/Assets/RouletteController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RouletteController : MonoBehaviour
+{
+    public float inputRotSpeed = 10.0f;
+
+    private float rotSpeed = 0.0f;
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        inputRotSpeed = Random.Range(100.0f, 200.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            inputRotSpeed = Random.Range(100.0f, 200.0f);
+            this.rotSpeed = inputRotSpeed;
+        }
+
+        transform.Rotate(0.0f, 0.0f, this.rotSpeed);
+
+        this.rotSpeed *= 0.96f;
+    }
+}
---
+++ SwipeCar/Assets/CarController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    private float speed = 0.0f;
+    private Vector2 startPos;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            this.startPos = Input.mousePosition;
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            Vector2 endPos = Input.mousePosition;
+            float swipeLength = endPos.x - this.startPos.x;
+
+            this.speed = swipeLength / 1000.0f;
+
+            GetComponent<AudioSource>().Play();
+        }
+
+        transform.Translate(this.speed, 0.0f, 0.0f);
+        this.speed *= 0.98f;
+    }
+}
