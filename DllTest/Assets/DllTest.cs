e5cd0d8f38ec83461c3edd754be0c42ccd85301a
KangsanBae <warimo47@naver.com>
2020-07-28 09:06:23 +0900

    Still testing...

--- DllTest/Assets/DllTest.cs
+++ DllTest/Assets/DllTest.cs
@@ -8,11 +8,15 @@ public class DllTest : MonoBehaviour
     [DllImport("MySum.dll")]
     extern public static int mySum(int a, int b);
 
+    [DllImport("TCPDll.dll")]
+    extern public static int getMyID();
+
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("시작");
-        Debug.Log(mySum(2, 5));
+        Debug.Log("mySum() " + mySum(-1, 3));
+        Debug.Log("getMyID() " + getMyID());
         Debug.Log("종료");
     }
 
